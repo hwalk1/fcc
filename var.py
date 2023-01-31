452f2ddb8dba03a18a6148a6c8521717c50bf5fe
HaydenW <haydenw@stridesolutions.com.au>
2023-01-31 11:21:24 +1100

    Variables + DT

---
+++ var.py
@@ -0,0 +1,10 @@
+# Variables & Interpolation
+# 
+character_name = "Hayden"
+character_age = "10"
+print("there one was a man named " + character_name + ",")
+print("he was " + character_age + " years old")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ",")
+print("but didnt like being 35.")
