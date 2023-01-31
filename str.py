92efb8716c3909855188629dd069743f34c49be1
HaydenW <haydenw@stridesolutions.com.au>
2023-01-31 11:39:57 +1100

    Str Methods

---
+++ str.py
@@ -0,0 +1,21 @@
+# New line
+print("Hello \n World")
+
+# Str Print
+phrase = "Hello World"
+print(phrase)
+
+print(phrase.islower())
+# By deafult str methods are on self with empty parenthesis
+print(phrase.upper().isupper())
+
+# Length of string
+print(len(phrase))
+
+# Able to get first character in the string
+print(phrase[0])
+
+#Gives index of where the param chacter is in the str
+print(phrase.index("e"))
+
+print(phrase.replace("Hello","World"))
