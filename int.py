06a129dd244d4696b5ab3bdda36542cb84cb654e
HaydenW <haydenw@stridesolutions.com.au>
2023-02-02 13:28:26 +1100

    Int Methods

---
+++ int.py
@@ -0,0 +1,34 @@
+from math import *
+
+# Has order of operations
+print(3 * 4 + 5)
+
+# Modulus
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+
+# Change to string
+print(str(my_num))
+
+# Power
+print(pow(3, 2))
+
+# Will return largest number, can also be min to return smallest
+print(max(3,4))
+
+print(round(3.2))
+
+
+# With Math Import
+# Will truncate to DP
+print(floor(3.7))
+# Round Up
+print(ceil(3.7))
+
+
+
+
+
+
